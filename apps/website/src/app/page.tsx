e60c4281a036399305914e8d645a23c73d000e98
abdellah <Abdellah.chehri14@gmail.com>
2024-01-05 19:41:39 +0100

    push to main

--- apps/website/src/app/page.tsx
+++ apps/website/src/app/page.tsx
@@ -6,7 +6,7 @@ import TextTyper from "../components/text-typer";
 export default function Page() {
   return (
     <MaxWidthWrapper className="mt-8">
-      <div className=" isolate w-full  bg-white pt-20 px-6 lg:px-0">
+      <div className=" isolate w-full  hidden lg:block bg-white pt-20 px-6 lg:px-0">
         {/* this is the title  */}
         <div className="mx-auto w-full place-items-center grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10">
           <div className="lg:col-span-2  lg:col-start-1 lg:row-start-1 lg:ml-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 ">
@@ -29,7 +29,7 @@ export default function Page() {
                 >
                   ابدء تجربتك المجانية
                 </Button>
-                <span className="mt-6 text-xl leading-8  block text-gray-700">
+                <span className="mt-6   text-xl leading-8  block text-gray-700">
                   14 يومًا تجريبيًّا
                 </span>
               </div>
@@ -37,7 +37,7 @@ export default function Page() {
           </div>
           {/* <WaterDropGrid /> */}
 
-          <div className="-ml-12 -mt-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 ">
+          <div className="-ml-12 -mt-12    lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 ">
             <img
               className="w-[32rem] max-w-none rounded-xl    sm:w-[48rem]"
               src="/Snap.png"
@@ -51,6 +51,39 @@ export default function Page() {
         <div className="absolute bottom-0  w-fit -z-10 h-fit right-0 ">
           <Gradian width="1000" height="800" />
         </div>
+      </div>
+
+      {/* this is the mobil view */}
+      <div className="w-full h-[500px] lg:hidden flex flex-col justify-center gap-y-4">
+        <div className="mx-auto w-full place-items-center grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10">
+          <div className="lg:col-span-2  lg:col-start-1 lg:row-start-1 lg:ml-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 ">
+            <div className="lg:max-w-lg pr-4 ">
+              <h1 className="mt-12 text-[30px] text-center qatar-bold font-bold tracking-tight text-gray-900 sm:text-4xl">
+                إنشاء، بيع، إدارة <TextTyper />
+              </h1>
+              <h1 className="text-[30px] qatar-bold text-center font-bold tracking-tight text-gray-900 sm:text-4xl">
+                من مكان واحد، بسهولة كبيرة
+              </h1>
+              <p className="mt-6 text-md leading-[40px] text-center text-gray-700">
+                من البناء بلا برمجة إلى التسويق والبيع بدون خبرة، مساق توفّر لك
+                كل الأدوات التي تحتاجها لإنشاء منصتك التعليمية وتنمية أعمالك عبر
+                الإنترنت.
+              </p>
+              <div className="w-full my-4 h-[60px] flex flex-col items-center  justify-start gap-y-8">
+                <Button
+                  size="lg"
+                  className="bg-[#43766C] text-xl py-4 px-6 text-white font-bold "
+                >
+                  ابدء تجربتك المجانية
+                </Button>
+                <span className="mt-6   text-xl leading-8  block text-gray-700">
+                  14 يومًا تجريبيًّا
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* <WaterDropGrid /> */}
       </div>
     </MaxWidthWrapper>
   );
--- apps/website/src/components/nav-bar.tsx
+++ apps/website/src/components/nav-bar.tsx
@@ -25,15 +25,17 @@ export const NavBar: FC = ({}) => {
   return (
     <div className="w-full h-[120px] ">
       <div className="mx-auto w-full   lg:max-w-screen-2xl  px-2.5 md:px-20">
-        <div className=" flex items-center  bg-[#F8FAE5] justify-between rounded-xl abdullah-shadow gap-x-2 w-full h-[70px] lg:h-[100px] px-4 mt-8 ">
+        <div className=" flex items-center  bg-[#F8FAE5] justify-between rounded-xl abdullah-shadow gap-x-2 w-full h-[90px] lg:h-[100px] px-4 mt-8 ">
           {/* this section is for the logo */}
           <div className="w-[40%] h-full flex items-center justify-start ">
-            <span className="text-3xl text-[#43766C] font-bold ">جدارة </span>
+            <span className="text-3xl qatar-bold text-[#43766C] font-bold ">
+              جدارة{" "}
+            </span>
           </div>
 
           {/* this section is for the nav menu and the action button */}
 
-          <div className="w-[60%] h-full flex items-end ">
+          <div className="w-[60%] h-full hidden lg:flex items-end ">
             <div className="w-[70%] h-full flex items-center justify-start ">
               {links.map((item) => {
                 return (
@@ -50,11 +52,14 @@ export const NavBar: FC = ({}) => {
             <div className="w-[30%] h-full flex items-center justify-end">
               <Button
                 size="lg"
-                className="bg-[#43766C] text-xl py-3 px-4 text-white font-bold "
+                className="bg-[#43766C] qatar-semibold text-xl rounded-lg py-4 px-8 text-white "
               >
                 انشاء حساب
               </Button>
             </div>
+          </div>
+          <div className="w-[50%] lg:hidden h-full flex items-center justify-end pl-4">
+            menu
           </div>
         </div>
       </div>
