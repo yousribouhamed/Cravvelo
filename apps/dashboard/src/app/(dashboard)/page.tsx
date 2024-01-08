00c4f84d7ddfb20f757e997e4fe6ccf4ab8b5ba1
abdellah <Abdellah.chehri14@gmail.com>
2024-01-08 14:26:30 +0100

    push to main

--- apps/dashboard/src/app/(dashboard)/page.tsx
+++ apps/dashboard/src/app/(dashboard)/page.tsx
@@ -1,16 +1,7 @@
-import Image from "next/image";
-import { Button } from "@ui/components/button";
 import MaxWidthWrapper from "../../components/MaxWidthWrapper";
-import { Sidebar } from "@/src/components/SideBar";
 import Header from "@/src/components/Header";
-import { prisma } from "database/src/index";
 
 export default async function Home() {
-  const data = await prisma.user.findMany();
-
-  console.log("here it is your data ");
-  console.log(data);
-
   return (
     <MaxWidthWrapper>
       <main className="w-full flex ">
