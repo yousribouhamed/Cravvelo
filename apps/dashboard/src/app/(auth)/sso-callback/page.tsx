2beaa94cc43ca64366c9afceb7e3ea4115f78cbe
abdellah <Abdellah.chehri14@gmail.com>
2024-01-12 00:21:54 +0100

    push to main

---
+++ apps/dashboard/src/app/(auth)/sso-callback/page.tsx
@@ -0,0 +1,20 @@
+import { type HandleOAuthCallbackParams } from "@clerk/types";
+
+import SSOCallback from "@/src/components/auth/sso-callback";
+
+// Running out of edge function execution units on vercel free plan
+// export const runtime = "edge"
+
+export interface SSOCallbackPageProps {
+  searchParams: HandleOAuthCallbackParams;
+}
+
+export default function SSOCallbackPage({
+  searchParams,
+}: SSOCallbackPageProps) {
+  return (
+    <div>
+      <SSOCallback searchParams={searchParams} />
+    </div>
+  );
+}
