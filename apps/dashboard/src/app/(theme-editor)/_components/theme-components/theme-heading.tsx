19589db5cf1eba74ba6bc9b8f4a1084ba64b57b4
abdellah <Abdellah.chehri14@gmail.com>
2024-02-06 14:36:10 +0100

    push to main

--- apps/dashboard/src/app/(theme-editor)/_components/theme-components/theme-heading.tsx
+++ apps/dashboard/src/app/(theme-editor)/_components/theme-components/theme-heading.tsx
@@ -42,7 +42,7 @@ const ThemeHeading: FC = ({}) => {
             </a>
             <a
               href="#"
-              className="rounded-md bg-blue-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500"
+              className="rounded-xl bg-blue-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500"
             >
               ابدأ الآن
             </a>
