968bbefd536388c97da89cbdca7ebaf9e98ce0e3
abdellah <Abdellah.chehri14@gmail.com>
2024-02-08 23:04:49 +0100

    push to main

--- apps/dashboard/src/app/(theme-editor)/layout.tsx
+++ apps/dashboard/src/app/(theme-editor)/layout.tsx
@@ -1,28 +1,15 @@
-import type { FC } from "react";
-import { Skeleton } from "@ui/components/ui/skeleton";
+import MaxWidthWrapper from "@/src/components/MaxWidthWrapper";
 
-const Loading: FC = ({}) => {
+export default async function EditorLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <div className="w-full flex flex-col h-screen ">
-      <div className="bg-white border-b flex justify-between items-center h-[60px] px-4 ">
-        <Skeleton className="h-[30px] border-b  w-[30px] " />
-        <Skeleton className="h-[30px] border-b  w-[150px] " />
-        <Skeleton className="h-[30px] border-b  w-[200px] " />
-      </div>
-      <div className="w-full flex h-full ">
-        <div className="w-[25%] max-w-[350px] border-l flex flex-col gap-y-4  p-4  h-full ">
-          <Skeleton className="h-[20px] border-b  w-full " />
-          <Skeleton className="h-[80px] border-b  w-full " />
+    <div className="flex w-full h-screen overflow-hidden  bg-white  light ">
+      {/* <ViewSwitcher /> */}
 
-          <Skeleton className="h-[150px] border-b  w-full " />
-        </div>
-
-        <div className="w-[75%] flex-grow relative   h-full bg-white p-4">
-          <Skeleton className="rounded-xl w-full h-full" />
-        </div>
-      </div>
+      <main className=" w-full h-full  ">{children}</main>
     </div>
   );
-};
-
-export default Loading;
+}
