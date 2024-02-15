6a06479804ef73c1ad688c7f0f6e13766edf7722
abdellah <Abdellah.chehri14@gmail.com>
2024-02-15 16:57:12 +0100

    push to main

--- apps/dashboard/src/app/(dashboard)/layout.tsx
+++ apps/dashboard/src/app/(dashboard)/layout.tsx
@@ -1,4 +1,4 @@
-import { Sidebar } from "@/src/components/sidebar";
+import { Sidebar } from "../../components/sidebar";
 import { ClerkProvider } from "@clerk/nextjs";
 import Script from "next/script";
 
@@ -11,11 +11,9 @@ export default async function AdminLayout({
     <ClerkProvider>
       <div className="flex w-full min-h-screen gap-x-2 bg-[#FAFAFA] light ">
         <Sidebar className="  lg:block lg:w-[250px] lg:fixed right-5  top-0  bottom-0 " />
-
         <main className=" w-full lg:w-[calc(100%-250px)] lg:mr-[250px]  px-10 ">
           {children}
         </main>
-        {/* <SubscripeButton /> */}
       </div>
       <Script src="//code.tidio.co/f4ntqdkrkcmkov2pbgapiuevig4fhtbq.js" async />
     </ClerkProvider>
