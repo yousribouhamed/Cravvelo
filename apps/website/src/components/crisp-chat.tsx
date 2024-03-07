496d5874b64a63d06c160d94eb76d3e3916fc368
abdellah <Abdellah.chehri14@gmail.com>
2024-03-07 09:18:33 +0100

    fix a bug

--- apps/website/src/components/crisp-chat.tsx
+++ apps/website/src/components/crisp-chat.tsx
@@ -5,7 +5,7 @@ import { Crisp, ChatboxColors, ChatboxPosition } from "crisp-sdk-web";
 
 export const CrispChat = () => {
   useEffect(() => {
-    Crisp.configure("8254f25a-eca0-421e-b4de-2bf655c03887");
+    Crisp.configure("8974edcb-dc2b-4c50-b2e3-4a4d22f9ef8f");
     Crisp.setColorTheme(ChatboxColors.Orange);
     Crisp.setPosition(ChatboxPosition.Left);
   }, []);
