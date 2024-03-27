383e995f52704ff3436eb5225ca7f29be74a60ba
abdellah <Abdellah.chehri14@gmail.com>
2024-03-27 15:38:30 +0100

    push to main

--- apps/website/src/components/drop-down-menu.tsx
+++ apps/website/src/components/drop-down-menu.tsx
@@ -1,5 +1,6 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import { Popover, Transition } from "@headlessui/react";
+
 import { ChevronDownIcon } from "lucide-react";
 import { buttonVariants } from "@ui/components/ui/button";
 import { cn } from "@ui/lib/utils";
@@ -131,49 +132,84 @@ const solutions = [
 export default function DropDownMenu() {
   return (
     <Popover className="relative">
-      <Popover.Button
-        className={cn(
-          buttonVariants({ variant: "ghost" }),
-          "font-semibold text-lg text-black leading-[40px]  rounded-full transition-all duration-150  "
-        )}
-      >
-        الخدمات
-        <ChevronDownIcon className="h-5 w-5" aria-hidden="true" />
-      </Popover.Button>
+      {({ open, close }) => (
+        <>
+          <Popover.Button
+            className={cn(
+              buttonVariants({ variant: "ghost" }),
+              "font-semibold text-lg text-black leading-[40px]  rounded-full transition-all duration-150  "
+            )}
+          >
+            الخدمات
+            <ChevronDownIcon
+              className={`${open ? "rotate-180" : "rotate-0"}
+           mr-2 h-5 w-5 transition duration-150 ease-in-out `}
+              aria-hidden="true"
+            />
+          </Popover.Button>
 
-      <Transition
-        as={Fragment}
-        enter="transition ease-out duration-200"
-        enterFrom="opacity-0 translate-y-1"
-        enterTo="opacity-100 translate-y-0"
-        leave="transition ease-in duration-150"
-        leaveFrom="opacity-100 translate-y-0"
-        leaveTo="opacity-0 translate-y-1"
-      >
-        <Popover.Panel className="absolute left-1/2 z-10 mt-5 flex w-screen max-w-max -translate-x-1/2 px-4">
-          <div className="w-screen max-w-[600px]  overflow-hidden rounded-3xl bg-white text-sm leading-6 shadow-lg ring-1 ring-gray-900/5 grid grid-cols-2 grid-rows-3 p-4">
-            {solutions.map((item) => (
-              <div
-                key={item.name}
-                className="group col-span-1 relative flex gap-x-6 rounded-lg p-4 hover:bg-gray-50"
-              >
-                <div className="mt-1 flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white shadow-lg">
-                  <item.icon aria-hidden="true" />
-                </div>
-                <div>
-                  <a href={item.href} className="font-semibold text-gray-900">
-                    {item.name}
-                    <span className="absolute inset-0" />
-                  </a>
-                  <p className="mt-1 text-gray-500 text-md">
-                    {item.description}
-                  </p>
-                </div>
+          <Popover.Overlay onClick={close} className="fixed inset-0 " />
+
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-200"
+            enterFrom="opacity-0 translate-y-1"
+            enterTo="opacity-100 translate-y-0"
+            leave="transition ease-in duration-150"
+            leaveFrom="opacity-100 translate-y-0"
+            leaveTo="opacity-0 translate-y-1"
+          >
+            <Popover.Panel className="absolute left-1/2 z-10 mt-5 flex w-screen max-w-max -translate-x-1/2 px-4">
+              <div className="w-screen max-w-[600px]  overflow-hidden rounded-3xl bg-white text-sm leading-6 shadow-lg ring-1 ring-gray-900/5 grid grid-cols-2 grid-rows-3 p-4">
+                {solutions.map((item) => (
+                  <div
+                    key={item.name}
+                    className="group col-span-1 group cursor-pointer relative flex gap-x-6 rounded-lg p-4 hover:bg-gray-50"
+                    onClick={close}
+                  >
+                    <div className="mt-1 flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white shadow-lg">
+                      <item.icon aria-hidden="true" />
+                    </div>
+                    <div>
+                      <a
+                        href={item.href}
+                        className="font-semibold text-gray-900 flex gap-x-2 group-hover:gap-x-3 transition-all duration-500 "
+                      >
+                        {item.name}
+                        <svg
+                          width="15"
+                          height="18"
+                          viewBox="0 0 15 18"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            d="M7.45166 4.74219L1.64342 10.5504L7.45166 16.3587"
+                            stroke="black"
+                            stroke-width="2"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                          />
+                          <path
+                            d="M1.64337 10.5498L14 10.5498"
+                            stroke="black"
+                            stroke-width="2"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                          />
+                        </svg>
+                      </a>
+                      <p className="mt-1 text-gray-500 text-md">
+                        {item.description}
+                      </p>
+                    </div>
+                  </div>
+                ))}
               </div>
-            ))}
-          </div>
-        </Popover.Panel>
-      </Transition>
+            </Popover.Panel>
+          </Transition>
+        </>
+      )}
     </Popover>
   );
 }
