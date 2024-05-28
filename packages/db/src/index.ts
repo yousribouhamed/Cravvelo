8bd79db8f9840538465f7e986c768c6e2792f1f5
abdellah <Abdellah.chehri14@gmail.com>
2024-05-28 06:51:58 +0200

    try to fix something

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,15 +1,33 @@
+// import { PrismaClient } from "@prisma/client";
+
+// const globalForPrisma = globalThis as unknown as {
+//   prisma: PrismaClient | undefined;
+// };
+
+// export const prisma =
+//   globalForPrisma.prisma ??
+//   new PrismaClient({
+//     log:
+//       //@ts-ignore
+//       process?.env?.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
+//   });
+// //@ts-ignore
+// if (process?.env?.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
+declare global {
+  // eslint-disable-next-line no-var
+  var cachedPrisma: PrismaClient;
+}
 
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log:
-      //@ts-ignore
-      process?.env?.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
-  });
-//@ts-ignore
-if (process?.env?.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+let prisma: PrismaClient;
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.cachedPrisma) {
+    global.cachedPrisma = new PrismaClient();
+  }
+  prisma = global.cachedPrisma;
+}
+
+export { prisma };
