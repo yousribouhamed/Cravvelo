7efdf61d0d4d2b65aab2d44b41eabd1bd579d66e
abdellah <Abdellah.chehri14@gmail.com>
2024-05-31 22:50:03 +0200

    some changes here and there

--- apps/dashboard/src/components/area-chart.tsx
+++ apps/dashboard/src/components/area-chart.tsx
@@ -115,7 +115,8 @@ const AreaChartOverview: FC<AreaChartProps> = ({ sales }: AreaChartProps) => {
               width={730}
               height={250}
               // aggregateSalesByMonth()
-              data={data}
+              // data={data}
+              data={aggregateSalesByMonth()}
               margin={{ top: 0, right: 0, left: 0, bottom: 0 }}
             >
               <defs>
