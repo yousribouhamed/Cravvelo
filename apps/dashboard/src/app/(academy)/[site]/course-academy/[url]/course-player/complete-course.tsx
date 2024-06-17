b2b407e500be04a870c65b71ac48762a65913df4
abdellah <Abdellah.chehri14@gmail.com>
2024-06-17 18:31:42 +0200

    push to main

--- apps/dashboard/src/app/(academy)/[site]/course-academy/[url]/course-player/complete-course.tsx
+++ apps/dashboard/src/app/(academy)/[site]/course-academy/[url]/course-player/complete-course.tsx
@@ -5,6 +5,8 @@ import { useCoursePlayerStore } from "@/src/app/(academy)/global-state/course-pl
 import { useState, type FC } from "react";
 import { Loader } from "lucide-react";
 import { revalidatePath } from "next/cache";
+import Link from "next/link";
+import { buttonVariants } from "@ui/components/ui/button";
 interface CompleteCourseProps {
   courseId: string;
   color: string;
@@ -24,29 +26,37 @@ const CompleteCourse: FC<CompleteCourseProps> = ({
       <span className="text-xl font-bold">
         {state?.currentModule?.title ?? "الانتقال الى الحلقة التالية"}
       </span>
-      <button
-        onClick={async () => {
-          setLoading(true);
-          try {
-            await updateStudentProgress({
-              courseId,
-            });
+      <div className="flex items-center gap-x-2 justify-end">
+        <button
+          onClick={async () => {
+            setLoading(true);
+            try {
+              await updateStudentProgress({
+                courseId,
+              });
 
-            await refetch();
-          } catch (err) {
-            console.error(err);
-          } finally {
-            window?.location?.reload();
-            setLoading(false);
-          }
-        }}
-        disabled={loading}
-        className="  rounded-xl w-fit text-white py-2 px-4 disabled:opacity-50 disabled:cursor-not-allowed cursor-pointer "
-        style={{ backgroundColor: color }}
-      >
-        {loading && <Loader className="w-4 h-4 ml-2 animate-spin" />}
-        أكمل واستمر
-      </button>
+              await refetch();
+            } catch (err) {
+              console.error(err);
+            } finally {
+              setLoading(false);
+            }
+          }}
+          disabled={loading}
+          className="  rounded-xl w-fit text-white py-2 px-4 disabled:opacity-50 disabled:cursor-not-allowed cursor-pointer "
+          style={{ backgroundColor: color }}
+        >
+          {loading && <Loader className="w-4 h-4 ml-2 animate-spin" />}
+          توثيق اكمال المشاهدة
+        </button>
+
+        <Link
+          href={"/"}
+          className={`${buttonVariants()} bg-black hover:bg-black text-white`}
+        >
+          العودة الى الاكادمية
+        </Link>
+      </div>
     </div>
   );
 };
