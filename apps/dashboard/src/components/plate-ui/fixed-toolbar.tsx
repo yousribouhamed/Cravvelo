d1a330065ab448d24bf81c3c468d937724f4b814
abdellah <Abdellah.chehri14@gmail.com>
2025-07-15 17:24:48 +0200

    feat: remove old editor

--- apps/dashboard/src/components/plate-ui/fixed-toolbar.tsx
+++
@@ -1,8 +0,0 @@
-import { withCn } from "@udecode/cn";
-
-import { Toolbar } from "./toolbar";
-
-export const FixedToolbar = withCn(
-  Toolbar,
-  "supports-backdrop-blur:bg-background/60 sticky left-0 -top-[15px] z-50 w-full h-fit justify-between overflow-x-auto   backdrop-blur"
-);
