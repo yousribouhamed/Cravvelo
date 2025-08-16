e7750cc8c75c945036cd1abe201506c26c91b03f
abdellah <Abdellah.chehri14@gmail.com>
2025-08-16 19:50:33 +0200

    remove api key

--- apps/academia/src/lib/resend.ts
+++ apps/academia/src/lib/resend.ts
@@ -1,6 +1,5 @@
 import { Resend } from "resend";
 
-const API_KEY =
-  process.env?.RESEND_API_KEY ?? "re_SjDH6ok1_HPtiDLWySobj2Uz7dZ68hFZT";
+const API_KEY = process.env?.RESEND_API_KEY ?? "";
 
 export const resend = new Resend(API_KEY);
